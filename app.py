114d8f21e993531dbb8378aa82b1eb6ba65a0644
Matt Goff <matt@goff.us>
2024-03-27 07:19:26 -0500

    wip

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from models.open_ai_model import OpenAIResponse
 
 load_dotenv()
 openai.api_key = os.getenv('OPENAI')
+OPENAI_TEMPERATURE = 0.7
 OPENAI_MODEL = 'gpt-3.5-turbo'
 
 
@@ -27,31 +28,31 @@ class Dialog:
         self.keep_going = False
 
     def get_response(self, model: str = OPENAI_MODEL) -> str:
-        result = openai.ChatCompletion.create(model=model, messages=self.dialog)
+        result = openai.ChatCompletion.create(model=model, messages=self.dialog, temperature=OPENAI_TEMPERATURE)
         response = OpenAIResponse(**result)
         return response.choices[0].message.content
 
     def interact(self) -> None:
-        print("Commands: \n\tSEND to send question.\n\tCLEAR to start a new conversation.\n\tDUMP to show the entire "
-              "conversation thead.\n\tEXIT to end the program.")
+        print("Commands: \n\t/send to send question.\n\t/clear to start a new conversation.\n\t/dump to show the entire "
+              "conversation thead.\n\t/exit to end the program.")
         while self.keep_going:
             print("Enter your question: ")
             lines = []
             while True:
                 line = input()
-                if line == "SEND":
+                if line == "/send":
                     break
-                if line in ["CLEAR", "DUMP", "EXIT"]:
+                if line in ["/clear", "/dump", "/exit"]:
                     lines.append(line)
                     break
                 lines.append(line)
             question = " ".join(lines)
 
-            if question == "CLEAR":
+            if question == "/clear":
                 self.clear()
-            elif question == "DUMP":
+            elif question == "/dump":
                 self.dump()
-            elif question == "EXIT":
+            elif question == "/exit":
                 self.keep_going = False
             else:
                 self.append("user", question)
